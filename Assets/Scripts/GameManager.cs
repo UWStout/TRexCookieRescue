50ee3a2660e95993ed1ba053e6610cc17116f9a6
christianhill1 <60521439+christianhill1@users.noreply.github.com>
2020-02-10 14:10:49 -0600

    a
    a

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static int score;
+    private Text scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText = GetComponent<Text>();//initialize the score to the text variable
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = "Score: " + score;//update the score to display the players score
     }
 }
--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -5,20 +5,17 @@ using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
-    public static int score;
-    int getScore = global.score;
-    private Text scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
-        scoreText = GetComponent<Text>();//initialize the score to the text variable
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = "Score: " + score;//update the score to display the players score
+
     }
 
 }
