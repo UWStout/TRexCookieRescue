3c2687486cd7942c8a097cdef122778b12568dc9
christianhill1 <60521439+christianhill1@users.noreply.github.com>
2020-02-10 13:57:47 -0600

    christian
    git might be down

--- Assets/Scripts/CookieScript.cs
+++ Assets/Scripts/CookieScript.cs
@@ -22,13 +22,14 @@ public class CookieScript : MonoBehaviour
         if (col.gameObject.name == "ChocoloateChip")//if the player touches a chocolate chip
         {
             //increase score by +1
-            global.score += 1;
+            ScoreScript.score += 1;
         }
 
         if (col.gameObject.name == "ChocoloateChunk")//if the player touches a chocolate chunk
         {
             //increase score by +10
-            global.score += 10;
+            //global.score += 10;
+            ScoreScript.score += 10;
         }
 
         if (col.gameObject.name == "DinosaurToy")//if the player touches a dinosaur toy
