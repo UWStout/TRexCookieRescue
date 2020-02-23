536f1e170d170a57f1dc64b0e0ef0ddb7d0166b2
Penrose3 <casword62@gmail.com>
2020-02-22 21:31:24 -0600

    ALPHA 0.1.1
    Quick bugfix, would've caused major memory leak.

--- Assets/Scripts/destroyObject.cs
+++ Assets/Scripts/destroyObject.cs
@@ -6,6 +6,7 @@ public class destroyObject : StateMachineBehaviour
 {
   public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
   {
+    Destroy(animator.gameObject.transform.parent.gameObject, stateInfo.length);
     Destroy(animator.gameObject, stateInfo.length);
     Debug.Log("Destroy!");
   }
